d48c6ee565117d13d38fe7c8d034e6f3d5d39f77
KimHyenam <iloveisyou@hanmir.com>
2021-10-03 18:13:14 +0900

    study:211003

---
+++ Vuejs/vue38컴포넌트-스타일/styleprj01/src/App.vue
@@ -0,0 +1,35 @@
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+</style>
+
+<template>
+    <div id="app">
+        <h2>{{msg}}</h2>
+        <Child1 />
+        <Child2 />
+    </div>
+</template>
+
+<script>
+import Child1 from "./components/Child1.vue";
+import Child2 from "./components/Child2.vue";
+
+export default {
+    props:[],
+    data: function(){
+        return {
+            msg: "Welcome to Your Vue.js App",
+        }
+    },
+    components: {
+        Child1: Child1,
+        Child2: Child2,
+    }
+};
+</script>
---
+++ Vuejs/vue38컴포넌트-스타일/styleprj01/src/components/Child1.vue
@@ -0,0 +1,20 @@
+<style scoped>
+.main {
+    border: 1px solid black;
+    background-color: yellow;
+}
+</style>
+<template>
+    <div class="main">
+        {{ msg }}
+    </div>
+</template>
+<script>
+export default {
+    data: function () {
+        return {
+            msg: "Child1.안녕하세요"
+        };
+    }
+};
+</script>
